b0d17e77c922bb0e75309ff60549a6b01ec935ea
CNLARS <53753876+CNLARS@users.noreply.github.com>
2020-05-27 12:17:52 -0700

    Code Clean Up
    +Removed Comments
    -Thumbnails in index.pug error

--- app.js
+++ app.js
@@ -4,19 +4,12 @@ const app = express();
 const data = require("./data.json");
 const bodyParser = require("body-parser");
 const projects = data.projects;
-//const indexRouter = require('./routes/index');
+const localNum = 3000; //Not associated with Hal 3000
 
 app.use(bodyParser.urlencoded( {extended: false} ) );
 
-
-//const citadelView = require("./views");
-//const aboutView = require(".views/about");
-//const indexView = require(".views/index");
-//const projectView = require(".views/project");
-
 //Sets the view engine to pug to use pug templates
 app.set("view engine", "pug");
-//app.use("/", indexRouter);
 
 //Sets static path for routing to the "public" folder:
 app.use("/static", express.static("public"));
@@ -24,13 +17,11 @@ app.use("/static", express.static("public"));
 //A route to RENDER "Home" page:
 app.get("/", (req, res) => {
     res.render("index");
-    //console.log(projects[1].project_name); //Testing123
 });
 
 //A route to RENDER the "about" page:
 app.get("/about", (req, res) => {
     res.render("about");
-    //console.log(projects[2].project_name); //Testing123
 });
 
  /*
@@ -68,7 +59,6 @@ app.use( (err, req, res, next) => {
 });
 
 
-const localNum = 3000; //Not associated with Hal 3000
 app.listen(localNum, () => {
     console.log(`Brought to you by LocalHost: ${localNum}`);
 });
